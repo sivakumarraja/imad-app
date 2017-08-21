58d42186331621083e23d3af2bd02380998e551e
Siva Kumar Raja Pagala <sivakumarraja@gmail.com>
2017-08-21 11:56:43 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -11,9 +11,9 @@ var img = document.getElementById('madi');
 var marginLeft = 0;
 function moveRight() {
     marginLeft = marginLeft + 1;
+    img.style.marginLeft = marginLeft + 'px';
     screen.height + "</p>";
 img += "<p>Available width/height: " + screen.availWidth + "*" + screen.availHeight + "</p>";
-    img.style.marginLeft = marginLeft + 'px';
 }
 img.onclick = function (){
     var interval = setInterval(moveRight,100);
