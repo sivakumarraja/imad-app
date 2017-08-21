eaafddae14a2f41fdefed3625a0f56c1b2562334
Siva Kumar Raja Pagala <sivakumarraja@gmail.com>
2017-08-21 12:18:36 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -12,9 +12,9 @@ var marginLeft = 0;
 function moveRight() {
     marginLeft = marginLeft + 10;
     img.style.marginLeft = marginLeft + 'px';
-    if(marginLeft > 1024 )  {
-        marginLeft = -1024;
-    }
+    //(marginLeft > 1024 )  {
+       //arginLeft = -1024;
+   // }
 }
 img.onclick = function (){
     var interval = setInterval(moveRight,100);
