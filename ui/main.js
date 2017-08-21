1f4ae2e196b041004328a511144524e1ded99dd1
Siva Kumar Raja Pagala <sivakumarraja@gmail.com>
2017-08-21 11:35:40 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
 console.log('Loaded!');
 
 // change the text of the main-text div
-var element = document.getElementByID('main-text'
+var element = document.getElementById('main-text'
 );
 element. innerHTML = "New Value";
 
