46649ba909e1459a04d662b89fce9f6de98ab245
Siva Kumar Raja Pagala <sivakumarraja@gmail.com>
2017-08-21 12:39:18 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -88,7 +88,7 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
-app.get('/ui/main.js', function (req, res) {
+app.get('/ui/siva-main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main-siva.js'));
 });
 
