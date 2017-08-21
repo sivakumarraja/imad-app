97c2a4b4c71f6cebbdc2f154a617f33eddeaa706
Siva Kumar Raja Pagala <sivakumarraja@gmail.com>
2017-08-21 08:30:23 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,13 @@
 console.log('Loaded!');
+
+//change the text of the main-text div
+var element = document.getElementById('main-text');
+
+element.innerHTML = "New Text added in client-side Java Script";
+
+//Move the image
+var img = document.getElementById('madi');
+
+img.onClick = function () {
+    img.style.marginLeft = '100px';
+};
