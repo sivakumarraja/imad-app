039003fe5d3cbe6d6c11759eaf89ffc8b1910728
Siva Kumar Raja Pagala <sivakumarraja@gmail.com>
2017-08-21 11:42:50 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,5 +9,5 @@ element. innerHTML = "New Value";
 
 var img = document.getElementById('madi');
 img.onclick = function (){
-    img.style.marginleft = '100px';
+    img.style.marginLeft = '100px';
 };
