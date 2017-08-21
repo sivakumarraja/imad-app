aaf1d29b008123a9da2d9bf8b071fa740d0f30ed
Siva Kumar Raja Pagala <sivakumarraja@gmail.com>
2017-08-21 08:39:00 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,14 +7,13 @@ element.innerHTML = "New Text added in client-side Java Script";
 
 //Move the image
 var img = document.getElementById('madi');
-
 var marginLeft = 0;
 function moveRight() {
     marginLeft = marginLeft + 1;
     img.style.marginLeft = marginLeft + 'px';
 }
-
 img.onClick = function () {
     //img.style.marginLeft = '100px';
+    alert('Click');
     var interval = setInterval(moveRight,50);
 };
