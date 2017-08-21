90e57c5580ac0e6f5e3b2aecc8701e6884857127
Siva Kumar Raja Pagala <sivakumarraja@gmail.com>
2017-08-21 12:43:48 -0700

    [imad-console] Updates ui/main-siva.js

--- ui/main-siva.js
+++ ui/main-siva.js
@@ -1,23 +1,23 @@
 console.log('Loaded 1 !');
 
 // change the text of the main-text div
-var element1 = document.getElementById('main-text1'
+var element = document.getElementById('main-text1'
 );
-element1. innerHTML = "New Value1";
+element. innerHTML = "New Value1";
 
 // Move the image
 
-var img1 = document.getElementById('madi1');
-var marginLeft1 = 0;
+var img = document.getElementById('madi1');
+var marginLeft = 0;
 function moveRight() {
-    marginLeft1 = marginLeft1 + 10;
-    img.style.marginLeft = marginLeft1 + 'px';
-    if(marginLeft1 > screen.width )  {
-        marginLeft1 = 0;
+    marginLeft = marginLeft + 10;
+    img.style.marginLeft = marginLeft + 'px';
+    if(marginLeft > screen.width )  {
+        marginLeft = 0;
     }
 }
 
-img1.onclick = function (){
+img.onClick = function (){
     var interval = setInterval(moveRight,50);
     
 };
