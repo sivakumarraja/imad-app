49278be9562d9f245eb1db0737113a3ec4f4233d
sivakumarraja <sivakumarraja@gmail.com>
2017-08-21 12:13:59 -0700

    added files for Siva

---
+++ ui/main-siva.js
@@ -0,0 +1,22 @@
+console.log('Loaded!');
+
+// change the text of the main-text div
+var element = document.getElementById('main-text'
+);
+element. innerHTML = "New Value";
+
+// Move the image
+
+var img = document.getElementById('madi');
+var marginLeft = 0;
+function moveRight() {
+    marginLeft = marginLeft + 10;
+    img.style.marginLeft = marginLeft + 'px';
+    if(marginLeft > 1024 )  {
+        marginLeft = -10;
+    }
+}
+img.onclick = function (){
+    var interval = setInterval(moveRight,100);
+    
+};
