833708b2c963a1e955173baff6f90d93e8c346cb
Siva Kumar Raja Pagala <sivakumarraja@gmail.com>
2017-08-21 07:22:33 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -76,7 +76,6 @@ app.get('/:articleName',function(req, res) {
     res.send(createTemplate(articles[articleName]));
 });
 
-
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
