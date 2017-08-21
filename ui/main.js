fff787773f7bbb297dc24264fa9170a3173ba1b8
Siva Kumar Raja Pagala <sivakumarraja@gmail.com>
2017-08-21 11:39:12 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,3 +5,9 @@ var element = document.getElementById('main-text'
 );
 element. innerHTML = "New Value";
 
+// Move the image
+
+var img = document.getElementById('img');
+imag.onclick = function (){
+    img.style.marginleft = '100px';
+};
