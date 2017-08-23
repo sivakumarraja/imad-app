4889217f1cbd557da1484476f4ebd0581c7198d6
Siva Kumar Raja Pagala <sivakumarraja@gmail.com>
2017-08-23 09:54:12 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -31,14 +31,32 @@ var nameInput = document.getElementById('name');
 var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function () {
-    // Make a request to the server and send the name
     
-    // capture a list of names and render it as a list
-    var names = ['name1','name2', 'name3', 'name4'];
-    var list = '';
-    for (var i=0; i< names.length; i++) {
-        list += '<li>' + names[i] + '</li>';
-    }
-    var ul = document.getElementById('namelist');
-    ul.innerHTML = list;
+    // Creat a request object
+    var request = new XMLHttpRequest();
+    
+    
+    // Capture the response and store it in a variable
+    request.onreadystatechange = function(){
+        if (request.readyState === XMLHttpRequest.DONE) {
+            // Take some action
+            if(request.status === 200) {
+                // capture a list of names and render it as a list
+                 var names = request.responseText;
+                names =JSON.parse(names);
+                var list = '';
+                for (var i=0; i< names.length; i++) {
+                    list += '<li>' + names[i] + '</li>';
+                }
+                var ul = document.getElementById('namelist');
+                ul.innerHTML = list;
+            }
+        }
+        // Not done yet
+    };
+    
+    // Make the request
+    request.open('GET','http://sivakumarraja.imad.hasura-app.io/submit-name=' + name, true);
+    request.send(null);
+    
 };
