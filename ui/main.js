07a81c30e640c6727dffc181527027f36f1ba733
Siva Kumar Raja Pagala <sivakumarraja@gmail.com>
2017-08-23 12:49:52 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -41,7 +41,9 @@ submit.onclick = function () {
             if(request.status === 200) {
                 // capture a list of names and render it as a list
                  var names = request.responseText;
+                 alert(names);
                 names =JSON.parse(names);
+                alert(names);
                 var list = '';
                 for (var i=0; i< names.length; i++) {
                     list += '<li>' + names[i] + '</li>';
