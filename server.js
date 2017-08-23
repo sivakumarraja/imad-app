de4c115ee2c2dddfaa97fb3a4181eb96ce657cb9
Siva Kumar Raja Pagala <sivakumarraja@gmail.com>
2017-08-23 13:25:28 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -79,7 +79,7 @@ app.get('/', function (req, res) {
 });
 
 var counter = 0;
-app.get('/counter', function(req, res) {
+app.get('/counter11', function(req, res) {
     counter = counter + 1;
     res.send(counter.toString());
 });
