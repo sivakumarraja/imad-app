287e62c6ef9f40fdfd7d9604b29a4c569fea323b
Siva Kumar Raja Pagala <sivakumarraja@gmail.com>
2017-08-23 12:34:35 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -74,6 +74,7 @@ function createTemplate(data){
                         <div id="comments">
                         </div>
                     </div>
+                    <script type="text/javascript" src="/ui/main.js"></script>
                 </div>
             </body>
         </html>
@@ -117,40 +118,6 @@ app.get('/:articleName',function(req, res) {
 });
 
 
-//submit name
-var submit = document.getElementById('comment-submit');
-submit.onclick = function () {
-    
-    // Creat a request object
-    var request = new XMLHttpRequest();
-    
-    
-    // Capture the response and store it in a variable
-    request.onreadystatechange = function() {
-        if (request.readyState === XMLHttpRequest.DONE) {
-            // Take some action
-            if(request.status === 200) {
-                // capture a list of names and render it as a list
-                 var comments = request.responseText;
-                comments =JSON.parse(comments);
-                var list = '';
-                for (var i= comments.length-1; i>=0; i--) {
-                    list += '<span>' + comments[i] + '</span>';
-                }
-                var ul = document.getElementById('comments');
-                ul.innerHTML = list;
-            }
-        }
-        // Not done yet
-    };
-    
-    // Make the request
-    var commentInput = document.getElementById('comment');
-    var comment = commentInput.value;
-    request.open('GET','http://sivakumarraja.imad.hasura-app.io/:articleName', true);
-    request.send(null);
-    
-};
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
