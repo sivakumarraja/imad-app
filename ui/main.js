d950289359c2e4ca55b7d7d434bcbeaccc159a9c
Siva Kumar Raja Pagala <sivakumarraja@gmail.com>
2017-08-23 07:38:47 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,15 +1,27 @@
 // Counter code
 var button = document.getElementById("counter");
-var counter = 0;
 
 button.onclick = function () {
     
-    // Make a request to the counter endpoint
+    // Creat a request object
+    var request = new XMLHttpRequest();
+    
     
     // Capture the response and store it in a variable
+    request.onreadystatechange = function(){
+        if (request.readystate --- XMLHttpRequest.DONE) {
+            // Take some action
+            if(request/status === 200) {
+                var counter = request.responsetext;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+        }
+        // Not done yet
+    };
     
-    // Render the variable in the correct span
-    counter = counter + 1;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
+    // Make the request
+    request.open('GET','http://sivakumarraja.imad.hasura-app.io/counter', true);
+    request.send(null);
+    
 };
