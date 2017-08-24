bb27c62476e95287330978b406be54054adaa4e9
Siva Kumar Raja Pagala <sivakumarraja@gmail.com>
2017-08-24 13:13:08 -0700

    [imad-console] Updates ui/main-siva.js

--- ui/main-siva.js
+++ ui/main-siva.js
@@ -16,8 +16,9 @@ submit.onclick = function () {
                 comments =JSON.parse(comments);
                 alert(comments);
                 var list = '';
-                for (var i= comments.length-1; i>=0; i+2) {
-                    list += '<span><b>' + comments[i+1] + '</b>:' + comments[i] + '</span><br/>';
+                for (var i= comments.length-1; i>=0; i++) {
+                    list += '<span><b>' + comments[i] + '</b>:' + comments[i+1] + '</span><br/>';
+                    i++;
                 }
                 var commentslist = document.getElementById('comments');
                 commentslist.innerHTML = list;
