ac8b25b5c440397c6a536289f8871b8cb955794f
Siva Kumar Raja Pagala <sivakumarraja@gmail.com>
2017-08-29 13:47:25 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -136,6 +136,37 @@ app.post('/create-user', function(req, res) {
     });
 })
 
+
+app.post('/login', function(req, res) {
+    var username = req.body.username;
+    var password = req.body.password;
+    
+    
+    pool.query('SELECT * from "user" where username = $1,', [username], function (err, result) {
+        if(err) {
+            res.status(500).send(err.toString());
+        } else {
+            if(result.rows.length === 0) {
+                res.status(403).send('username/password is invalid');
+            }
+            else
+            {
+                //Match the password
+                var dbString = result.rows[0].password
+                dbString.split('$')[2]; //salt value;
+                var hashedPassword = hash(password, salt); //creating a hash based password submitted and the original salt
+                if(hashedPassword === dbString)
+                {
+                    res.send("Credentials correct"); 
+                } else {
+                    res.status(403).send('username/password is invalid');
+                }
+                res.send('User successfully created: ' + username);
+            }
+        }
+    });
+})
+
 var pool = new Pool(config);
 app.get('/test-db', function(req,res){
     //make a select request
