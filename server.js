0772bbeae1923f3c2ceddc284e8ce54049e9a44d
Siva Kumar Raja Pagala <sivakumarraja@gmail.com>
2017-08-29 13:36:36 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -119,7 +119,7 @@ app.get('/hash/:input', function(req, res) {
 //app.get('/create-user', function(req, res) {
 app.post('/create-user', function(req, res) {
     // username, password
-    
+    // {"username": "siva:=", "password": "password"}
     //JSON Request
     var username = req.body.username;
     var password = req.body.password;
