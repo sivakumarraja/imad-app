23dac309cfa7774834d5c8bdce86f3bb6f47bd1f
Siva Kumar Raja Pagala <sivakumarraja@gmail.com>
2017-08-29 13:31:12 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
 var crypto = require('crypto');
+var bodyParser = requie('body-parser');
 
 var config = {
     user: 'sivakumarraja',
@@ -14,6 +15,7 @@ var config = {
 
 var app = express();
 app.use(morgan('combined'));
+app.use(bodyParser.json());
 
 /*var articles={ 
     'article-one':{
@@ -117,10 +119,14 @@ app.get('/hash/:input', function(req, res) {
 //app.get('/create-user', function(req, res) {
 app.post('/create-user', function(req, res) {
     // username, password
+    
+    //JSON Request
     var username = req.body.username;
     var password = req.body.password;
+    
     var salt = crypto.getRandomBytes(128).toString("hex");
     var dbString = hash(passowrd, salt);
+    
     pool.query('INSERT INTO "user" (username, password) VALUES ($1, $2)', [username, dbString], function (err, result) {
         if(err) {
             res.status(500).send(err.toString());
